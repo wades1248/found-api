5a01cb4da0423a9df48db3f5e804b28036ea50f2
wades1248 <wades1248@gmail.com>
2019-09-28 17:10:33 -0500

    re-added ssl

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,5 +6,6 @@ module.exports = {
     "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
+    "ssl": !!process.env.SSL,
 }
     
