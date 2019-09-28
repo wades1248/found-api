07d9cde771dd8f45d74065c947280ae47711c06c
wades1248 <wades1248@gmail.com>
2019-09-28 16:42:58 -0500

    prepping for db deployment

--- test/app.spec.js
+++ test/app.spec.js
@@ -15,7 +15,7 @@ describe(`items endpoints`, function(){
     before(`make knex instance`, () =>{
         db = knex({
             client: 'pg',
-            connection: process.env.TEST_DB_URL
+            connection: process.env.TEST_DATABASE_URL
         })
         app.set('db', db)
     })
